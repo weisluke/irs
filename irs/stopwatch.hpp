5bd10518613ccd78658c57659322aa827aee0aa1
Luke Weisenbach <luke.weisenbach@port.ac.uk>
2023-08-23 17:58:59 +0100

    add stopwatch file

---
+++ irs/stopwatch.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <chrono>
+
+class Stopwatch
+{
+    std::chrono::high_resolution_clock::time_point tstart;
+    std::chrono::high_resolution_clock::time_point tend;
+
+public:
+
+    void start()
+    {
+        tstart = std::chrono::high_resolution_clock::now();
+    }
+
+    double stop(bool reset = true)
+    {
+        tend = std::chrono::high_resolution_clock::now();
+        double dt = std::chrono::duration_cast<std::chrono::milliseconds>(tend - tstart).count() / 1000.0;
+
+        if (reset)
+        {
+            tstart = std::chrono::high_resolution_clock::time_point();
+            tend = std::chrono::high_resolution_clock::time_point();
+        }
+
+        return dt;
+    }
+
+};
+

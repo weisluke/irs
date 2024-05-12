a6922d3ef57b9850d324bf7ae12088c0d4603177
Luke Weisenbach <lukeweisenbach@live.com>
2024-05-12 18:50:10 +0100

    add comments and change input parameters

--- irs/mass_functions/mass_function_base.cuh
+++ irs/mass_functions/mass_function_base.cuh
@@ -16,23 +16,46 @@ public:
 	calculate mass drawn from a mass function given a probability.
 
 	\param p -- number drawn uniformly in [0,1]
+	\param m_lower -- lower mass cutoff for the distribution in arbitrary units
+	\param m__upper -- upper mass cutoff for the distribution in arbitrary units
 	******************************************************************************/
-	__host__ __device__ virtual T mass(T p, ...) {} = 0;
+	__host__ __device__ virtual T mass(T p, T m_lower, T m_upper)
+	{
+		return 0;
+	}
 
 	/******************************************************************************
 	calculate <mass> for a mass function
+
+	\param m_lower -- lower mass cutoff for the distribution in arbitrary units
+	\param m__upper -- upper mass cutoff for the distribution in arbitrary units
 	******************************************************************************/
-	__host__ __device__ virtual T mean_mass(...) {} = 0;
+	__host__ __device__ virtual T mean_mass(T m_lower, T m_upper)
+	{
+		return 0;
+	}
 
 	/******************************************************************************
 	calculate <mass^2> for a mass function
+
+	\param m_lower -- lower mass cutoff for the distribution in arbitrary units
+	\param m__upper -- upper mass cutoff for the distribution in arbitrary units
 	******************************************************************************/
-	__host__ __device__ virtual T mean_mass2(...) {} = 0;
+	__host__ __device__ virtual T mean_mass2(T m_lower, T m_upper)
+	{
+		return 0;
+	}
 
 	/******************************************************************************
 	calculate <mass^2 ln(mass)> for a mass function
+
+	\param m_lower -- lower mass cutoff for the distribution in arbitrary units
+	\param m__upper -- upper mass cutoff for the distribution in arbitrary units
 	******************************************************************************/
-	__host__ __device__ virtual T mean_mass2_ln_mass(...) {} = 0;
+	__host__ __device__ virtual T mean_mass2_ln_mass(T m_lower, T m_upper)
+	{
+		return 0;
+	}
 
 };
 

5e05caf7197a56c139ecc0547745c8311435a513
Luke Weisenbach <lukeweisenbach@live.com>
2024-05-12 18:52:20 +0100

    uniform just needs to initalize slope to 0

--- irs/mass_functions/uniform.cuh
+++ irs/mass_functions/uniform.cuh
@@ -6,61 +6,19 @@
 namespace massfunctions{
     
 /******************************************************************************
-template class for handling equal mass point mass lenses
+template class for handling point mass lenses with a uniform distribution
 ******************************************************************************/
 template <typename T>
 class Uniform : public PowerLaw<T>
 {
-
-private:
-	T slope = static_cast<T>(0);
 	
 public:
 
-    /******************************************************************************
-	calculate mass drawn from a mass function given a probability.
-
-	\param p -- number drawn uniformly in [0,1]
-	\param m_lower -- lower mass cutoff for the distribution in arbitrary units
-	\param m__upper -- upper mass cutoff for the distribution in arbitrary units
-	******************************************************************************/
-	__host__ __device__ T mass(T p, T m_lower, T m_upper, ...)
-	{
-		return PowerLaw::mass(p, m_lower, m_upper, slope);
-	}
-
 	/******************************************************************************
-	calculate <mass> for a mass function
-
-	\param m_lower -- lower mass cutoff for the distribution in arbitrary units
-	\param m__upper -- upper mass cutoff for the distribution in arbitrary units
+	a uniform distribution is a power law with slope 0
+	no further functions needed as they are not modified from the base class
 	******************************************************************************/
-	__host__ __device__ T mean_mass(T m_lower, T m_upper, ...)
-	{
-		return PowerLaw::mean_mass(m_lower, m_upper, slope);
-	}
-
-	/******************************************************************************
-	calculate <mass^2> for a mass function
-
-	\param m_lower -- lower mass cutoff for the distribution in arbitrary units
-	\param m__upper -- upper mass cutoff for the distribution in arbitrary units
-	******************************************************************************/
-	__host__ __device__ T mean_mass2(T m_lower, T m_upper, ...)
-	{
-		return PowerLaw::mean_mass2(m_lower, m_upper, slope);
-	}
-
-	/******************************************************************************
-	calculate <mass^2 ln(mass)> for a mass function
-
-	\param m_lower -- lower mass cutoff for the distribution in arbitrary units
-	\param m__upper -- upper mass cutoff for the distribution in arbitrary units
-	******************************************************************************/
-	__host__ __device__ virtual T mean_mass2_ln_mass(T m_lower, T m_upper, ...)
-    {
-		return PowerLaw::mean_mass2_ln_mass(m_lower, m_upper, slope);
-    }
+	__host__ __device__ Uniform() : PowerLaw<T>(0) {};
 
 };
 
